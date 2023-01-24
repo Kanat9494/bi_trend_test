c37de9fdaaa00be55db5a725ffe2460d542b2afa
Kanat9494 <kkanat94@gmail.com>
2023-01-24 22:57:30 +0600

    moving

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,3 +1,7 @@
 const appname = "BiTrend";
 const appversion = "Version 1.0.0";
-const credits = "@Baaba Devs";
+const credits = "@Leto Inc";
+const email = "Email";
+const emailHint = "admin@admin.kg";
+const password = "Пароль";
+const passwordHint = "********";
---
+++ lib/widgets_common/custom_textfield.dart
@@ -0,0 +1,10 @@
+import 'package:bi_trend_test/consts/consts.dart';
+
+Widget customTextField() {
+  return Column(
+    children: [
+      email.text.color(green45).fontFamily(semibold).size(16).make(),
+      5.heightBox,
+    ],
+  );
+}
